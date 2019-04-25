a9ba793f4c34172c6793fa5c11b86db82a87275d
武新飞 <xinfei.wu@qq.com>
2019-04-26 00:36:09 +0800

    test eventemitter

--- mediasoup/event_emitter_test.go
+++ mediasoup/event_emitter_test.go
@@ -3,28 +3,84 @@ package mediasoup
 import (
 	"testing"
 
-	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestEventEmitter(t *testing.T) {
-	called := 0
-	fn := func(i int) {
-		called++
-	}
+func TestEventEmitter_AddListener(t *testing.T) {
 	evName := "test"
-	logger := logrus.WithField("type", "eventEmitter")
-	emiter := NewEventEmitter(logger)
+	logger := TypeLogger("eventEmitter")
+	emitter := NewEventEmitter(logger)
 
-	emiter.AddListener(evName, fn)
-	emiter.SafeEmit(evName)
-	assert.Equal(t, called, 1)
+	emitter.AddListener(evName, 1, "a", func() {}, func() {})
+	assert.Equal(t, 2, emitter.ListenerCount(evName))
+}
 
-	emiter.SafeEmit(evName, "argument type invalid")
-	assert.Equal(t, called, 1)
+func TestEventEmitter_Once(t *testing.T) {
+	evName := "test"
+	logger := TypeLogger("eventEmitter")
+	emitter := NewEventEmitter(logger)
 
-	emiter.RemoveListener(evName, fn)
-	emiter.SafeEmit(evName)
-	assert.Equal(t, called, 1)
-	assert.Equal(t, emiter.ListenerCount(evName), 0)
+	onceObserver := NewMockFunc(t)
+	emitter.Once(evName, onceObserver.Fn())
+	emitter.Emit(evName)
+	emitter.Emit(evName)
+
+	assert.Equal(t, 1, onceObserver.CalledTimes())
+}
+
+func TestEventEmitter_Emit(t *testing.T) {
+	evName := "test"
+	logger := TypeLogger("eventEmitter")
+	emitter := NewEventEmitter(logger)
+
+	onObserver := NewMockFunc(t)
+	emitter.On(evName, onObserver.Fn())
+	emitter.Emit(evName)
+	emitter.Emit(evName)
+
+	assert.Equal(t, 2, onObserver.CalledTimes())
+}
+
+func TestEventEmitter_SafeEmit(t *testing.T) {
+	evName := "test"
+	logger := TypeLogger("eventEmitter")
+	emitter := NewEventEmitter(logger)
+
+	called := false
+	emitter.On(evName, func(int) { called = true })
+	emitter.SafeEmit(evName, "1")
+
+	assert.False(t, called)
+}
+
+func TestEventEmitter_RemoveListener(t *testing.T) {
+	evName := "test"
+	logger := TypeLogger("eventEmitter")
+	emitter := NewEventEmitter(logger)
+
+	onObserver := NewMockFunc(t)
+	fn := onObserver.Fn()
+
+	emitter.On(evName, fn)
+	emitter.RemoveListener(evName, fn)
+	emitter.Emit(evName)
+
+	assert.Equal(t, 0, onObserver.CalledTimes())
+	assert.Equal(t, 0, emitter.ListenerCount(evName))
+}
+
+func TestEventEmitter_RemoveAllListeners(t *testing.T) {
+	evName := "test"
+	logger := TypeLogger("eventEmitter")
+	emitter := NewEventEmitter(logger)
+
+	onObserver := NewMockFunc(t)
+	fn := onObserver.Fn()
+
+	emitter.On(evName, fn)
+	emitter.RemoveAllListeners(evName)
+	emitter.Emit(evName)
+
+	assert.Equal(t, 0, onObserver.CalledTimes())
+	assert.Equal(t, 0, emitter.ListenerCount(evName))
 }
