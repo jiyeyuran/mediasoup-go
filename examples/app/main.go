f1b61227736fa92a9c12fed5dac24e9dc1c7cde4
wuxinfei <xinfei.wu@qq.com>
2020-10-10 20:13:30 +0800

    git

---
+++ examples/app/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"encoding/json"
+
+	"github.com/jiyeyuran/mediasoup"
+)
+
+var logger = mediasoup.NewLogger("ExampleApp")
+
+func main() {
+	worker, err := mediasoup.NewWorker()
+	if err != nil {
+		panic(err)
+	}
+	worker.On("@failure", func(err error) {
+		logger.Error("%s", err)
+	})
+	worker.On("died", func(err error) {
+		logger.Error("%s", err)
+	})
+	worker.On("@success", func() {
+		dump, err := worker.Dump()
+		if err != nil {
+			panic(err)
+		}
+		logger.Debug("dump: %s", dump)
+
+		usage, err := worker.GetResourceUsage()
+		if err != nil {
+			panic(err)
+		}
+		data, _ := json.Marshal(usage)
+		logger.Debug("usage: %s", data)
+	})
+
+	wait := make(chan struct{})
+	<-wait
+}
